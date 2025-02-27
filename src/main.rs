aea234c1b69c7e6b27a1a45a19154b99453170b1
Marcin Siwak <marcin.siwak@gmail.com>
2025-02-27 19:02:55 +0100

    The echo builtin

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,26 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+        match Command::from(input) {
+            Command::Echo(echo) => println!("{echo}"),
+            Command::Exit(return_code) => std::process::exit(return_code),
+            Command::Unknown(unknown_command) => println!("{}: command not found", unknown_command),
+        }
+    }
+}
+
+enum Command {
+    Unknown(String),
+    Echo(String),
+    Exit(i32),
+}
+
+impl From<String> for Command {
+    fn from(input: String) -> Command {
         match input.trim() {
-            "exit 0" => std::process::exit(0),
-            _ => println!("{}: command not found", input.trim()),
+            "exit 0" => Command::Exit(0),
+            echo if echo.starts_with("echo ") => Command::Echo(input[4..].trim().to_string()),
+            other => Command::Unknown(other.to_string()),
         }
     }
 }
