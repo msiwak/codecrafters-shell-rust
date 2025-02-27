a8ff1340f2d67f0bc952a6540c385155b6fcd3a5
Marcin Siwak <marcin.siwak@gmail.com>
2025-02-27 18:34:51 +0100

    Handle invalid commands

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    println!("{}: command not found", input.trim());
 }
